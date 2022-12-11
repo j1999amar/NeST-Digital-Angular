a3786ad128c112062f4c559c395e2d75b5f51c24
Amarnath <88896502+j1999amar@users.noreply.github.com>
2022-12-11 11:22:58 +0530

    Admin Login

---
+++ src/app/admin-login/admin-login.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-admin-login',
+  templateUrl: './admin-login.component.html',
+  styleUrls: ['./admin-login.component.css']
+})
+export class AdminLoginComponent {
+  constructor(private route:Router){}
+ data:any={}
+  adminUserLogin=''
+  adminPassword=''
+  readValue=()=>{
+    this.data={
+      "adminUserLogin":this.adminUserLogin,
+      "adminPassword":this.adminPassword
+    }
+    if(this.adminUserLogin=="admin"&&this.adminPassword=="12345") 
+    {
+      this.route.navigate(['/addEmployee'])
+      alert()
+    }else{
+      alert("Login Failed!!!")
+    }
+    
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { AdminLoginComponent } from './admin-login/admin-login.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
+const myRouter:Routes=[
+  {
+    path:'',
+    component:AdminLoginComponent
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminLoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,FormsModule,RouterModule.forRoot(myRouter),HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
